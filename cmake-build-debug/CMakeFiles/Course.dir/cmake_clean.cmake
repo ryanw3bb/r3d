8dd461c1ea8cb42c75634042157b3514b5a09c80
Ryan Webb <ryan@ryanwebb.com>
2018-06-09 11:15:57 +0100

    removing unneeded files

--- cmake-build-debug/CMakeFiles/Course.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Course.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/Course.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Course.
-# This may be replaced when dependencies are built.
